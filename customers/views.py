f6dc4a16ce956e7617ef468ed94dec7fc059a660
Isaac Cavalcante <48898725+isaaccavalcantef@users.noreply.github.com>
2024-09-23 01:43:14 -0300

    Chega mais uma vez

--- customers/views.py
+++ customers/views.py
@@ -23,14 +23,11 @@ class CustomerOrdersView(DetailView):
 		customers = Customers.objects.filter(id=pk).values
 		orders = Orders.objects.filter(customer_id=pk)
 		ordersCount = Orders.objects.filter(customer_id=pk).count
-		ordersSoma = Orders.objects.filter(customer_id=pk)
-		soma = Orders.objects.filter(customer_id=pk).annotate(sum('valor'))
 		context = super(CustomerOrdersView, self).get_context_data(**kwargs)
 		context['Orders'] = orders
 		context['customers'] = customers
 		context['pk'] = pk
 		context['count'] = ordersCount
-		context['sum'] = soma
 		return context
 
 
